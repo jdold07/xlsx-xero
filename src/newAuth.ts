fe95a5237eaa95dca33f93ba6e2a74dfbe18e5ba
jdold07 <tim@tpstech.dev>
2023-03-25 10:47:43 +1000

    feat: ✨🎉 initial commit of auth function for new Xero tenant
    - Initial commit of "single" use feature for authorising the initial or new tenants.
    - Not pretty or refined, a bit clunky, but it serves my purpose for now.
    - Generates the auth link and opens the URL in the default browser.
    - Waits for the user to authorise the app via Xero process in browser.
    - Asks user to enter the copied redirect URL from the browser.
    - Generates the Xero Token Set & Updates the Tenant array to include the new tenant.

---
+++ src/newAuth.ts
@@ -0,0 +1,71 @@
+import { config } from "dotenv"
+import open from "open"
+import Readline from "readline/promises"
+import { XeroClient } from "xero-node"
+import { writeTokenSetJson } from "./helpers"
+
+newAuth()
+
+/**
+ * Gets a new tokenSet from Xero API and saves it to the database.
+ * Required for authorises new Organisations to use the app.
+ * @param {string} userId
+ * @returns {Promise<void>}
+ * @notes Seperate function only required initially or to authorise new Organisations.
+ */
+async function newAuth() {
+  config()
+  const client_id = process.env.XERO_CLIENT_ID ?? ""
+  const client_secret = process.env.XERO_CLIENT_SECRET ?? ""
+  const redirectUris = [process.env.XERO_REDIRECT_URI ?? ""]
+  const scopes = (process.env.XERO_SCOPES ?? "").split(" ")
+
+  // Initialize the XeroClient and build the consent url
+  const xero = new XeroClient({
+    clientId: client_id,
+    clientSecret: client_secret,
+    redirectUris,
+    scopes
+  })
+  await xero.initialize()
+  const consentUrl = await xero.buildConsentUrl()
+  console.log(consentUrl)
+
+  // Open the consent url in the user's browser
+  open(consentUrl)
+
+  // Create a readline interface to get the callback url from the user
+  const rl = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  console.log(
+    "\n1. Follow the steps in your browser to authorise a new Organisation.\n",
+    "2. When it completes, copy the callback url from the address bar of your browser.\n",
+    "3. Then return to this terminal and paste the callback url below.\n"
+  )
+  const url = await rl.question("What is the callback url? ")
+  rl.close()
+
+  // Get the tokenSet from Xero API using the callback url from the user input
+  const tokenSet = await xero.apiCallback(url)
+
+  // Save the new tokenSet to the database
+  if (await writeTokenSetJson(tokenSet)) {
+    console.log(
+      "\nXero tokenSet saved to the database.\n",
+      "Any newly authorised Organisations will be available in the Tenant array.\n",
+      "Make sure to update switch controls and package.json scripts to run a new Tenant.\n"
+    )
+
+    process.exitCode = 0
+  } else {
+    console.warn(
+      "\nXero tokenSet was NOT saved to the database.\n",
+      "You will need to re-run the newAuth script again to save authorised Organisations to the Tenant array and database.\n"
+    )
+    process.exitCode = 1
+  }
+  return
+}
