b9a0926c8f9ab36424b64a21f9beb316344dfa98
jdold07 <tim@tpstech.dev>
2023-03-26 22:40:54 +1000

    refactor: 🔥 remove unused protgres db queries for handling token set

--- src/dbQueries.ts
+++ src/dbQueries.ts
@@ -1,6 +1,5 @@
-import { TokenSet } from "xero-node"
 import db from "./dbServer"
-import { ChargesAndPaymentsObj, ChargeWithCustomer, ValidTokenSet } from "./types"
+import { ChargesAndPaymentsObj, ChargeWithCustomer } from "./types"
 
 export async function fetchChargesfromDB(xlsxCharges: ChargesAndPaymentsObj[]) {
   const dbCharges = [] as ChargeWithCustomer[]
@@ -143,92 +142,6 @@ export async function fetchDDInvoiceData(date: string | Date) {
     const totalCustCharges = charges.reduce((a, c) => a + +c.amount, 0)
 
     return { ...gmTotals, storeExp, deptSales, ddTitle, totalCustCharges }
-  } catch (error: any) {
-    console.error(error?.message ?? JSON.stringify(error))
-    await db.$disconnect()
-    throw new Error(error?.message ?? JSON.stringify(error))
-  }
-}
-
-export async function fetchTokenSetFromDB(userId: string | undefined) {
-  const tokenSet = await db.xeroTokenStore.findUnique({
-    where: {
-      userId: userId,
-    },
-    select: {
-      id_token: true,
-      access_token: true,
-      refresh_token: true,
-      expires_in: true,
-      expires_at: true,
-      token_type: true,
-      scope: true,
-    },
-  })
-  if (!tokenSet) {
-    console.error("No token set found in DB")
-    throw new Error("No token set found in DB")
-  }
-
-  const validTokenSet: ValidTokenSet = Object.fromEntries(
-    Object.entries(tokenSet).filter((k, v) => v !== null)
-  )
-
-  if (!validTokenSet) {
-    console.error("Xero tokenSet retrieved from db is empty!")
-    throw new Error("Xero tokenSet retrieved from db is empty!")
-  }
-  console.log("A valid Xero tokenSet was retrieved from the database")
-  return validTokenSet
-}
-
-/**
- * *Store the tokenSet in the database*
- * Write the tokenSet to the database
- * @param {string} userId
- * @param {TokenSet} tokenSet
- * @returns {Promise<boolean>} true on success | throws error on failure
- */
-export async function saveTokenSetInDb(
-  userId: string | undefined,
-  tokenSet: TokenSet
-): Promise<boolean> {
-  try {
-    if (
-      !tokenSet ||
-      !tokenSet.id_token ||
-      !tokenSet.access_token ||
-      !tokenSet.expires_in ||
-      !tokenSet.refresh_token ||
-      !tokenSet.scope ||
-      !userId
-    ) {
-      console.error("TokenSet is missing required fields")
-      throw new Error("TokenSet is missing required fields")
-    }
-    await db.xeroTokenStore.upsert({
-      where: { userId },
-      update: {
-        id_token: tokenSet.id_token,
-        access_token: tokenSet.access_token,
-        expires_in: tokenSet.expires_in,
-        expires_at: tokenSet.expires_at ?? Date.now() + tokenSet.expires_in * 1000,
-        refresh_token: tokenSet.refresh_token,
-        token_type: tokenSet.token_type,
-        scope: tokenSet.scope,
-      },
-      create: {
-        userId,
-        id_token: tokenSet.id_token,
-        access_token: tokenSet.access_token,
-        expires_in: tokenSet.expires_in,
-        expires_at: tokenSet.expires_at ?? Date.now() + tokenSet.expires_in * 1000,
-        refresh_token: tokenSet.refresh_token,
-        token_type: tokenSet.token_type,
-        scope: tokenSet.scope,
-      },
-    })
-    return true
   } catch (error: any) {
     console.error(error?.message ?? JSON.stringify(error))
     await db.$disconnect()
